9ddbadd901affbd2f7d32629b9cb469cbbdffdf0
rachmatgunawan <rachmat.d.gunawan@gmail.com>
2019-12-28 22:03:04 +0700

    vscode
    Editor VScode

---
+++ Assets/Script/menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+// Update is called once per frame
+	void Update () {
+		
+	}
+	
+public void ke_kembali(){
+		Application.LoadLevel (0);
+	}
+
+public void ke_mulai(){
+		Application.LoadLevel (1);
+	}
+	
+public void ke_tentang(){
+		Application.LoadLevel (2);
+	}
+	
+public void ke_kredit(){
+		Application.LoadLevel (3);
+	}
+	
+public void ke_keluar(){
+		Application.Quit ();
+	}
+	
+
+}
